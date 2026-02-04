bc18c5fffde6bef3ed74f784ec5bb9384e0bd601
Cheon-dae-gwang <cjseorhkd@gmail.com>
2026-02-04 13:42:16 +0900

    Add additional print statements for developers

--- src/testprj/Main.java
+++ src/testprj/Main.java
@@ -3,5 +3,7 @@ package testprj;
 public class Main {
 	private void sysout() {
 		System.out.println("개발자1이 개발한 내용입니다.");
+		System.out.println("개발자2가 개발한 내용입니다.");
+		System.out.println("개발자3이 개발한 내용입니다.");
 	}
 }
